2b1bcc5ea11b18e28e4c834ce162243df9251512
vitor-passos <vitorpassos16@gmail.com>
2023-06-20 01:07:13 -0300

    connection with postgres

---
+++ db/connection.go
@@ -0,0 +1,20 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+func OpenConnection() (*sql.DB, error) {
+	sc := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", "localhost", "5432", "client_infos", "1231", "client")
+	conn, err := sql.Open("postgres", sc)
+	if err != nil {
+		panic(err)
+	}
+
+	err = conn.Ping()
+	return conn, err
+}
+
